1b614ad578c270d4d636f6399cf48ca3bcf7b8db
Onyedikachi123 <onyedikachierugo@gmail.com>
2022-07-13 00:19:43 +0100

    fixed  Sequelize Connection Error

--- config/db.js
+++ config/db.js
@@ -11,10 +11,5 @@ module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+  ssl: true,
 });
