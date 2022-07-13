20ab1f71a8899ff2c51aa682a7754ec30119a2e1
Onyedikachi123 <onyedikachierugo@gmail.com>
2022-07-14 00:23:02 +0100

    updating the whatsapp route

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,9 +1,32 @@
+const { default: axios } = require("axios");
 const express = require("express");
 
 const router = express.Router();
 
-router.post("/webhook", (req, res) => {
+router.post("/webhook", async (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
+  const [message] = req.body.messages;
+  const [contact] = req.body.contacts;
+  const text = message.text.body;
+  const phone = contact.wa_id;
+
+  const payload = {
+    recipient_type: "individual",
+    to: phone,
+    type: "text",
+    text: {
+      body: "Hello, dear customer!",
+    },
+  };
+  const res = await axios.post(
+    "https://waba-sandbox.360dialog.io/v1/messages",
+    payload,
+    {
+      header: {
+        "D360-API-KEY": "emr1IC_sandbox",
+      },
+    }
+  );
   res.status(200).send("Success");
 });
 
