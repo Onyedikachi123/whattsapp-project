7fa6736859305759c95098098138ca24c15fd520
Onyedikachi123 <onyedikachierugo@gmail.com>
2022-07-14 00:41:41 +0100

    updating the route

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -4,34 +4,40 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/webhook", async (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2));
-  const [message] = req.body.messages;
-  const [contact] = req.body.contacts;
-  const text = message.text.body;
-  const phone = contact.wa_id;
+  try {
+    console.log(JSON.stringify(req.body, null, 2));
+    const [message] = req.body.messages;
+    const [contact] = req.body.contacts;
+    const text = message.text.body;
+    const phone = contact.wa_id;
 
-  console.log({
-    message, contact
-  })
-  const payload = {
-    recipient_type: "individual",
-    to: phone,
-    type: "text",
-    text: {
-      body: "Hello, dear customer!",
-    },
-  };
-  const _res = await axios.post(
-    "https://waba-sandbox.360dialog.io/v1/messages",
-    payload,
-    {
-      headers: {
-        "D360-API-KEY": "emr1IC_sandbox",
+    console.log({
+      message,
+      contact,
+    });
+    const payload = {
+      recipient_type: "individual",
+      to: phone,
+      type: "text",
+      text: {
+        body: "Hello, dear customer!",
       },
-    }
-  );
-  console.log(_res)
-  res.status(200).send("Success");
+    };
+    const _res = await axios.post(
+      "https://waba-sandbox.360dialog.io/v1/messages",
+      payload,
+      {
+        headers: {
+          "D360-API-KEY": "emr1IC_sandbox",
+        },
+      }
+    );
+    console.log(_res);
+    res.status(200).send("Success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
 });
 
 module.exports = router;
